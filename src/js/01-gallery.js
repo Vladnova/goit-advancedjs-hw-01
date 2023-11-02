5640f4e36f1055e514274024818c05d02e804f83
Vladyslav Novalenko <vladyslav.novalenko@infotech.gov.ua>
2023-11-02 21:04:36 +0200

    finish first home work

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ description, original, preview }) => `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src=${preview}
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </li>`
+    )
+    .join("");
+}
+
+list.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+
+new SimpleLightbox(".gallery a", {captionsData:'alt', captionDelay: 250});
