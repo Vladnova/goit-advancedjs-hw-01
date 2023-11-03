fc056888aab2445f07addd42f1ef67cd4da5ca80
Vladyslav Novalenko <vladyslav.novalenko@infotech.gov.ua>
2023-11-03 12:12:42 +0200

    finished second home work

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const TIME_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000) );
+
+function onPlay(data) {
+  localStorage.setItem(TIME_KEY, JSON.stringify(data.seconds) )
+}
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(TIME_KEY)) );
---
+++ src/js/newFile.js
@@ -0,0 +1,19 @@
+import { index } from './02-video';
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  // const onPlay = function(data) {
+  //   player.setCurrentTime(data.seconds).then(function(seconds) {
+  //   console.log(seconds)
+  //   localStorage.setItem('videoplayer-current-time', seconds)
+  // }).catch(function(error) {
+  //   switch (error.name) {
+  //       case 'RangeError':
+  //           // the time was less than 0 or greater than the video’s duration
+  //           break;
+  //       default:
+  //           // some other error occurred
+  //           break;
+  //   }
+  // });
+}
