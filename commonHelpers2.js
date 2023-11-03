992956c60750a8071e69901f73df700d48bd7431
Vladnova <61030713+Vladnova@users.noreply.github.com>
2023-11-03 10:13:04 +0000

    Deploying to gh-pages from @ Vladnova/goit-advancedjs-hw-01@fc056888aab2445f07addd42f1ef67cd4da5ca80 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as a}from"./assets/vendor-78be7656.js";const n=document.querySelector("iframe"),e=new r(n),t="videoplayer-current-time";e.on("timeupdate",a(i,1e3));function i(o){localStorage.setItem(t,JSON.stringify(o.seconds))}e.setCurrentTime(JSON.parse(localStorage.getItem(t)));
+//# sourceMappingURL=commonHelpers2.js.map
