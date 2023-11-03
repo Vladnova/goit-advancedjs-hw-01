f8e067c4fb434f722de4cad3f3b88c40a649f7e3
Vladyslav Novalenko <vladyslav.novalenko@infotech.gov.ua>
2023-11-03 15:13:28 +0200

    finished the third home work

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const FORM_DATA_KEY = 'feedback-form-state';
+const { email, message } = form.elements;
+const localStorGet = localStorage.getItem(FORM_DATA_KEY);
+
+form.addEventListener('input', throttle(handlerInput, 500));
+form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(e) {
+  e.preventDefault();
+
+  const data = {};
+  const formData = new FormData(e.currentTarget);
+
+  formData.forEach((value, name) => {
+    data[name] = value;
+  });
+
+  if (data.email === '' || data.message === '') {
+    return alert(
+      'To send the form, you must fill in all fields. Please fill them out'
+    );
+  }
+  console.log(data);
+  form.reset();
+  localStorage.removeItem(FORM_DATA_KEY);
+}
+
+function handlerInput(e) {
+  const dataSubmit = { email: email.value, message: message.value };
+  localStorage.setItem(FORM_DATA_KEY, JSON.stringify(dataSubmit));
+}
+
+if (localStorGet) {
+  const dataInLocalStore = JSON.parse(localStorGet);
+  email.value = dataInLocalStore.email;
+  message.value = dataInLocalStore.message;
+}
